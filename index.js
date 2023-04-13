e976df7537118dac079fdb4ded270356a4554a81
luis <luisguibarreto8@gmail.com>
2023-04-13 12:39:00 -0300

    adicionando comentários e finalizando o projeto

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+//Função cria uma variável para cada requisito
 function cadastrar() {
     let email = document.getElementById("email").value;
     let senha = document.getElementById("senha").value;
     let cpf = document.getElementById("cpf").value;
     let nome = document.getElementById("nome").value;
-
+//condição que verifica se o usuário preencheu todos os campos
     if (nome === "" || senha === "" || email === "" || cpf === "") {
         alert("Por favor, preencha todos os campos.");
         return false;
     } 
-    else if (senha.length < 6){
+ //condição que verifica se a senha tem mais de 6 caracteres   
+    else if (senha.length <= 6){
         alert("A senha deve conter mais de 6 caracteres")
     }
     else {
