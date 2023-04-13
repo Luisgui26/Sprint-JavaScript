ce1f3fbd16d57ab2389dae5f5e343244f04068b7
luis <luisguibarreto8@gmail.com>
2023-04-12 21:23:48 -0300

    Criando variáveis

---
+++ index.js
@@ -0,0 +1,6 @@
+function cadastrar() {
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+    let cpf = document.getElementById("cpf").value;
+    let nome = document.getElementById("nome").value;
+}
