05475ce6c481b0e90eb8025505e6fb730f2fca41
luis <luisguibarreto8@gmail.com>
2023-04-13 12:29:50 -0300

    Terminando de estilizar o projeto e adicionandoo readme

---
+++ README.txt
@@ -0,0 +1,9 @@
+Integrantes do grupo:
+Luís Guilherme Barreto RM:98688
+Leonardo Mansur RM:551659
+Arthur Abreu RM:98283
+Carlos Roberto RM:551877
+
+
+projeto está no perfil do github "Luisgui26" em um repositório com nome de "Sprint-JavaScript"
+
--- index.js
+++ index.js
@@ -7,7 +7,11 @@ function cadastrar() {
     if (nome === "" || senha === "" || email === "" || cpf === "") {
         alert("Por favor, preencha todos os campos.");
         return false;
-    } else {
+    } 
+    else if (senha.length < 6){
+        alert("A senha deve conter mais de 6 caracteres")
+    }
+    else {
         alert("Cadastro efetuado!")
     }
 
